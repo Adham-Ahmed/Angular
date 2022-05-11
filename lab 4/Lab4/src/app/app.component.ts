c65d6b692cd7fffd49052a9b067f5c48979dbb95
Adham <adhamahmed1234321@gmail.com>
2022-05-11 15:13:06 +0200

    department add and list done

--- lab 4/Lab4/src/app/app.component.ts
+++ lab 4/Lab4/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DepartmentService } from './department.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DepartmentService]
 })
 export class AppComponent {
   title = 'Lab4';
--- lab 4/Lab4/src/app/department/department.module.ts
+++ lab 4/Lab4/src/app/department/department.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DepartmentListComponent } from './department-list/department-list.component';
+import { DepartmentAddComponent } from './department-add/department-add.component';
+import { DepartmentDetailsComponent } from './department-details/department-details.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    DepartmentListComponent
+    DepartmentListComponent,
+    DepartmentAddComponent,
+    DepartmentDetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports:[
+    DepartmentListComponent,
+    DepartmentAddComponent,
+    DepartmentDetailsComponent
   ]
 })
 export class DepartmentModule { }
