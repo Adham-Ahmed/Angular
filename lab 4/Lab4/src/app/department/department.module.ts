112f159365b4d1c478432190ef83b5f468910e27
Adham <adhamahmed1234321@gmail.com>
2022-05-11 13:47:41 +0200

    department module and component created

---
+++ lab 4/Lab4/src/app/department/department.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DepartmentListComponent } from './department-list/department-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    DepartmentListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DepartmentModule { }
