9f922acc5fe214a0fcc577843d0d4984bb7f1747
Adham <adhamahmed1234321@gmail.com>
2022-05-12 12:09:58 +0200

    starting with lab 5

--- lab 4/Lab4/src/app/app.module.ts
+++ lab 4/Lab4/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentModule } from './department/department.module';
 import { StudentModule } from './student/student.module';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DepartmentModule,
-    StudentModule
+    StudentModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
